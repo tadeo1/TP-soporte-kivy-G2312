a46cadc39eb16c433df044c6a277deb9886b84a8
Tadeo Schlieper <tadeo.sch@hotmail.com>
2023-07-11 16:48:01 -0300

    borrado de productos

--- db.py
+++ db.py
@@ -33,7 +33,7 @@ def crear_producto(nombre, descripcion, categoria, precio, stock):
             (nombre, descripcion, categoria, precio, stock)
         )
         con.commit()
-        print("alta con exito", nombre, descripcion, categoria, precio, stock)
+        print("alta con exito:", nombre, descripcion, categoria, precio, stock)
     finally:
         con.close()
 
@@ -46,7 +46,22 @@ def buscar_producto_por_nombre(nombre):
             """SELECT id, nombre FROM producto WHERE nombre LIKE ?""",
             ("%" + nombre + "%",)
         )
-        print("busqueda con exito", nombre)
+        print("busqueda con exito:", nombre)
+        return cur.fetchall()
+    finally:
+        con.close()
+
+
+def borrar_producto_por_nombre(nombre):
+    con = sqlite3.connect("myapp.db")
+    cur = con.cursor()
+    try:
+        cur.execute(
+            """DELETE FROM producto WHERE nombre = ?""",
+            (nombre,)
+        )
+        con.commit()
+        print(f"'{nombre}' borrado con exito")
         return cur.fetchall()
     finally:
         con.close()
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 
 import db
 
@@ -65,7 +67,48 @@ class PaginaAlta(Screen):
 
 
 class PaginaBaja(Screen):
-    pass
+    caja_lista = ObjectProperty(None)
+    widgets = []
+
+    def listar_borrado_productos(self, termino_busqueda):
+        for w in self.widgets:
+            self.caja_lista.remove_widget(w)
+        self.widgets = []
+        resultado = db.buscar_producto_por_nombre(termino_busqueda)
+        for prod in resultado:
+            item = BoxLayout(
+                orientation="horizontal",
+                size_hint_y=None,
+                height=40,
+            )
+            boton_borrar = Button(
+                text="Borrar",
+                size_hint=(None, None),
+                height=40,
+                width=60,
+                # font_size=12,
+                # background_normal='borrar.png',
+                # background_down='borrar.png',
+                # size_hint=(.3, .3),
+                # pos_hint={"x": 0.35, "y": 0.3},
+                on_release=self.borrar_prod(prod[1], termino_busqueda)
+            )
+            label_nombre = Label(
+                text=f"{prod[1]}",
+                size_hint_y=None,
+                height=40,
+                # font_size=12,
+            )
+            item.add_widget(label_nombre)
+            item.add_widget(boton_borrar)
+            self.caja_lista.add_widget(item)
+            self.widgets.append(item)
+
+    def borrar_prod(self, nombre, termino_busqueda):
+        def func_borrar(*args):
+            db.borrar_producto_por_nombre(nombre)
+            self.listar_borrado_productos(termino_busqueda)
+        return func_borrar
 
 
 class WindowManager(ScreenManager):
